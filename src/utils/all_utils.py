c22bef2700444778e4cbb6117ca51b7ae533e75d
Chandra Reddy Tiyyagura <chandra.tiyyagura@gmail.com>
2022-09-28 11:52:58 +0530

    all utils file updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -9,5 +9,5 @@ def read_yaml(path_to_yaml: str)-> dict:
 
 def create_directory(dirs:list):
     for dir_path in dirs:
-        os.makedirs(dir_path, exists_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
         print(f"Directory is created at {dir_path}")
